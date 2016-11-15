05f330e45e67e26b38e43ae07e25bb669832351d
Andrew Brown <drusepth@gmail.com>
2016-11-15 23:56:12 +0100

    Wrap world around itself rather than expanding infinitely

--- app/models/soul.rb
+++ app/models/soul.rb
@@ -41,13 +41,13 @@ class Soul < ActiveRecord::Base
 
       # Claim soul bounties
       if other_soul.soul_bounty > 0
-        player.update_attribute :souls, player.souls + other_soul.soul_bounty
+        player.update_attribute :free_souls, player.free_souls + other_soul.soul_bounty
       end
     end
   end
 
   def level_up!
-    player.update_attribute :souls, player.souls + 1
+    player.update_attribute :free_souls, player.free_souls + 1
     self.level += 1
     self.health += HEALTH_PER_LEVEL_UP
   end
@@ -57,7 +57,7 @@ class Soul < ActiveRecord::Base
     self.alive = false
 
     # Refund the soul to its owner
-    player.update_attribute :souls, player.souls + 1
+    player.update_attribute :free_souls, player.free_souls + 1
   end
 
   private
@@ -66,15 +66,7 @@ class Soul < ActiveRecord::Base
     new_x_coord = x + (1 - rand(3))
     new_y_coord = y + (1 - rand(3))
 
-    update_attributes!({
-      x: new_x_coord,
-      y: new_y_coord
-    })
-
-    # self.x = new_x_coord
-    # self.y = new_y_coord
-
-    # self.save!
+    move_to new_x_coord, new_y_coord
   end
 
   def swarm_nearest_faction!
@@ -100,12 +92,7 @@ class Soul < ActiveRecord::Base
     new_y_coord += (1 - rand(2)) if soul_to_swarm_to.y > y
     new_y_coord -= (1 - rand(2)) if soul_to_swarm_to.y < y
 
-    # self.x = new_x_coord
-    # self.y = new_y_coord
-    update_attributes!({
-      x: new_x_coord,
-      y: new_y_coord
-    })
+    move_to new_x_coord, new_y_coord
   end
 
   def swarm_nearest_soul!
@@ -130,9 +117,13 @@ class Soul < ActiveRecord::Base
     new_y_coord += (1 - rand(2)) if soul_to_swarm_to.y > y
     new_y_coord -= (1 - rand(2)) if soul_to_swarm_to.y < y
 
+    move_to new_x_coord, new_y_coord
+  end
+
+  def move_to x, y
     update_attributes!({
-      x: new_x_coord,
-      y: new_y_coord
+      x: new_x_coord % world.width,
+      y: new_y_coord % world.height
     })
   end
 end
